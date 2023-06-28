7c1f6a4dbfb97526d3d69787561a0cb3d41b051f
DivineCrusade <DivineCrusade@DESKTOP-TFAFPE4>
2023-06-28 20:15:50 +0300

    feat: Implement executer of RPN expressions

--- Calculator/Unit1.cpp
+++ Calculator/Unit1.cpp
@@ -1,4 +1,5 @@
 #include "ConverterRPN.hpp"
+#include "ExecuterRPN.hpp"
 
 #include <Windows.h>
 #include <iostream>
@@ -7,18 +8,24 @@
 int main()
 {
     Calculator::ConverterRPN converter{ };
+    Calculator::ExecuterRPN executer{ };
     
-    std::string expr{ "3 + 4 * 2 / ( 1 - 5 ) ^ 2 ^ 3 " };
+    //std::string expr{ "3 + 4 * 2 / ( 1 - 5 ) " };
+    std::string expr{ "3 + 4 * 2 / ( 1 - 5 ) ^ 2 " };
+    //std::string expr{ "( 1 + 2 ) * 4 + 3 " };
 
     //std::cin >> std::noskipws >> expr;
 
     auto rpn_expr{ converter.convert_expr_with_notation(expr) };
-    while (!rpn_expr.empty())
-    {
-        std::cout << rpn_expr.front() << ' ';
-        rpn_expr.pop();
-    }
+    //while (!rpn_expr.empty())
+    //{
+    //    std::cout << rpn_expr.front() << ' ';
+    //    rpn_expr.pop();
+    //}
 
+    auto res{ executer.execute_expr_according_notation(rpn_expr) };
+
+    std::cout << res;
 
     return EXIT_SUCCESS;
 }
