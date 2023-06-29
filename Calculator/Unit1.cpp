b492902d88ff61f3ae9a20a6f7bcfc44b9212a32
Divinecrusade <gorlovdima@inbox.ru>
2023-06-29 11:54:14 +0300

    feat: Add class calculator consisting of converter and executer of math expressions

--- Calculator/Unit1.cpp
+++ Calculator/Unit1.cpp
@@ -1,31 +1,38 @@
 #include "ConverterRPN.hpp"
 #include "ExecuterRPN.hpp"
+#include "Calculator.hpp"
 
 #include <Windows.h>
 #include <iostream>
+#include <memory>
 
 
 int main()
 {
-    Calculator::ConverterRPN converter{ };
-    Calculator::ExecuterRPN executer{ };
+    //MathExprCalculator::ConverterRPN converter{ };
+    //MathExprCalculator::ExecuterRPN executer{ };
     
+    std::string expr{ "5^2+4*2/(2-5)" };
     //std::string expr{ "3 + 4 * 2 / ( 1 - 5 ) " };
-    std::string expr{ "3 + 4 * 2 / ( 1 - 5 ) ^ 2 " };
+    //std::string expr{ "3 + 4 * 2 / ( 1 - 5 ) ^ 2 " };
     //std::string expr{ "( 1 + 2 ) * 4 + 3 " };
 
     //std::cin >> std::noskipws >> expr;
 
-    auto rpn_expr{ converter.convert_expr_with_notation(expr) };
+    MathExprCalculator::Calculator<MathExpression::Notation::RPN> calculator{ std::make_shared<MathExprCalculator::ConverterRPN>(), std::make_shared<MathExprCalculator::ExecuterRPN>() };
+
+    //auto rpn_expr{ converter.convert_expr_with_notation(expr) };
     //while (!rpn_expr.empty())
     //{
     //    std::cout << rpn_expr.front() << ' ';
     //    rpn_expr.pop();
     //}
 
-    auto res{ executer.execute_expr_according_notation(rpn_expr) };
+    //auto res{ executer.execute_expr_according_notation(rpn_expr) };
 
-    std::cout << res;
+    std::cout << calculator.calculate(expr);
+
+    //std::cout << res;
 
     return EXIT_SUCCESS;
 }
