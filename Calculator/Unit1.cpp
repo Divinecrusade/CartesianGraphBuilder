df7319dfb7fc9bc6e3e891e148369a8061c1e182
DivineCrusade <DivineCrusade@DESKTOP-TFAFPE4>
2023-07-01 18:17:10 +0300

    fix: Error due passing only 1 number

--- Calculator/Unit1.cpp
+++ Calculator/Unit1.cpp
@@ -13,10 +13,11 @@ int main()
     //MathExprCalculator::ExecuterRPN executer{ };
     
     //std::string expr{ "5^2+4*2/(2-5)" };
-    std::string expr{ "1000--7" };
+    //std::string expr{ "1000--7" };
     //std::string expr{ "3" };
-    //std::string expr{ "3+4*2/(1-5)" };
-    //std::string expr{ "3 + 4 * 2 / ( 1 - 5 ) ^ 2 " };
+    //std::string expr{ "-3" };
+    //std::string expr{ "-3+4*2/(1-5)" };
+    std::string expr{ "3+4*2/(-1-5)^2" };
     //std::string expr{ "( 1 + 2 ) * 4 + 3 " };
 
     //std::cin >> std::noskipws >> expr;
