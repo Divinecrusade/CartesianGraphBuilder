91aed1e6df761e419708d1041ae211a619b4a468
Divinecrusade <gorlovdima@inbox.ru>
2024-01-20 19:24:07 +0300

    feat: Add PreCalculator class for constants and argument processing

--- GUI_Application/MainForm.cpp
+++ GUI_Application/MainForm.cpp
@@ -1,14 +1,13 @@
 #include "MainForm.h"
 #include "..\Calculator\Calculator.hpp"
+#include "PreCalculator.hpp"
 
 #include <Windows.h>
 
 using namespace GUIApplication;
 
-char tmp[] {'x', 'e'};
-
 std::pair<size_t const, char const* const> const Validater::calculator_symbols = std::make_pair(std::size(MathExprCalculator::Calculator<MathExpression::Notation::RPN>::available_symbols), MathExprCalculator::Calculator<MathExpression::Notation::RPN>::available_symbols);
-std::pair<size_t const, char const* const> const Validater::preprocessor_symbols = std::make_pair(std::size(tmp), tmp);
+std::pair<size_t const, char const* const> const Validater::precalculator_symbols = std::make_pair(std::size(PreCalculator<'x'>::available_symbols), PreCalculator<'x'>::available_symbols);
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
