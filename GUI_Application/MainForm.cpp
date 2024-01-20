74789a0ded335c25b3a4663e187ffeb4fd07dd9c
Divinecrusade <gorlovdima@inbox.ru>
2024-01-20 19:23:51 +0300

    refactor: Make validate function more readable

--- GUI_Application/MainForm.cpp
+++ GUI_Application/MainForm.cpp
@@ -7,8 +7,8 @@ using namespace GUIApplication;
 
 char tmp[] {'x', 'e'};
 
-std::pair<size_t, char const* const> const Validater::calculator_symbols = std::make_pair(std::size(MathExprCalculator::Calculator<MathExpression::Notation::RPN>::available_symbols), MathExprCalculator::Calculator<MathExpression::Notation::RPN>::available_symbols);
-std::pair<size_t, char const* const> const Validater::preprocessor_symbols = std::make_pair(std::size(tmp), tmp);
+std::pair<size_t const, char const* const> const Validater::calculator_symbols = std::make_pair(std::size(MathExprCalculator::Calculator<MathExpression::Notation::RPN>::available_symbols), MathExprCalculator::Calculator<MathExpression::Notation::RPN>::available_symbols);
+std::pair<size_t const, char const* const> const Validater::preprocessor_symbols = std::make_pair(std::size(tmp), tmp);
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
